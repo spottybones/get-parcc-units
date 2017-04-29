54b3fd944fb17edf752bf2e22161a477ef450021
Scott Burns <sburns@centercitypcs.org>
2017-04-29 09:16:50 -0400

    Shell Script for Querying Contents of TestNAV Cache
    Dump the contents page from a Pearson TestNAV ProctorCache server and
    extract the list of content units it contains to STDOUT.

---
+++ get-parcc-units.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# @(#) get-parcc-units.sh - dump the contents page from a PARCC cache
+# and extract a list of the Units it contains to stdout
+
+SRV_NAME=$1
+
+wget -O - http://${SRV_NAME}.ccpcs.local:4480/contents.jsp?locale=en_US \
+    | grep '<a href="contentdetails.jsp' \
+    | sed -E 's/^.*<a href="contentdetails.jsp.+>([^<]+)<.*$/\1/' \
+    | sort
